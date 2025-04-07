9fb9c73418327ed40187235f9a66b86b7eae9ab8
cjmamo <823038+cjmamo@users.noreply.github.com>
2025-04-07 11:57:36 +0200

    feat: implement org unit groups and org unit group set sync
    support multiple target servers
    
    add error handling
    
    test: use Playwright instead of Citrus for testing since the latter is hard to use

---
+++ java/TargetsSplitter.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.apache.camel.Exchange;
+
+public class TargetsSplitter {
+
+    private static final Pattern TARGET_PROPERTY_PATTERN = Pattern.compile("^target\\.([\\d]+)\\.(.+)");
+    private static final int PROPERTY_INDEX = 1;
+    private static final int PROPERTY_VALUE = 2;
+
+    public List<Map<String, String>> split(Exchange exchange) {
+        Map<Integer, Map<String, String>> targetsByIndex = new HashMap<>();
+
+        Map<String, Object> properties = exchange.getContext().getPropertiesComponent().loadPropertiesAsMap();
+        for (Map.Entry<String, Object> property : properties.entrySet()) {
+            Matcher matcher = TARGET_PROPERTY_PATTERN.matcher(property.getKey());
+            if (matcher.matches()) {
+                int index = Integer.parseInt(matcher.group(PROPERTY_INDEX));
+                Map<String, String> target = targetsByIndex.getOrDefault(index, new HashMap<>());
+                target.put(matcher.group(PROPERTY_VALUE), (String) property.getValue());
+                targetsByIndex.put(index, target);
+            }
+        }
+
+        List<Map<String, String>> targets = targetsByIndex.values().stream().collect(Collectors.toList());
+        for (Map<String, String> target : targets) {
+            target.putIfAbsent("idScheme", "uid");
+            target.putIfAbsent("endpointUri", "direct:dhis2Target");
+        }
+
+        return targets;
+    }
+
+
+}
+
---
+++ tests/reference-org-unit-sync.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+test('has org units synced', async ({ request }) => {
+
+    const orgUnit = await request.post('http://localhost:8080/api/organisationUnits', {
+        data: {
+          id: 'b7HFMWjj3im',
+          name: 'Acme',
+          code: 'ACME',
+          shortName: 'Acme',
+          openingDate: '2020-01-01'
+        },
+         headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q=',
+            },
+    });
+
+    expect(orgUnit.status()).toBe(201)
+
+    await expect.poll(async () => {
+      const response = await request.get('http://localhost:8081/api/organisationUnits/b7HFMWjj3im', {
+           headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q=',
+              }
+          });
+      return response.status();
+    }).toBe(200);
+
+    await expect.poll(async () => {
+      const response = await request.get('http://localhost:8082/api/organisationUnits/b7HFMWjj3im', {
+           headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q=',
+              }
+          });
+      return response.status();
+    }).toBe(200);
+});
